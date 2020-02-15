47b3cf616bd31c213c7930f53986badb6a6513a9
shafinsiddique <siddiqueshafin20@gmail.com>
2020-02-15 12:40:03 -0500

    backend monogodb interface completed.

---
+++ backend/db_helper.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+class DBHelper:
+    def __init__(self):
+        # set up the connection and the collection.
+        pass
+
+    def get_posts(self):
+        # return a list of all the post objects. List of dicitonaries
+        # [{"name":"my first blog", "date": "22 Oct, 1999", "id"=1}]
+        pass
+
+    def insert_post(self, post):
+        # insert <post> into the db.
+
+        pass
+
--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,5 @@ from flask import Flask
 import requests
 app = Flask(__name__)
 @app.route('/get_posts',methods=['GET'])
-
 def posts():
     return {"message":"success"}
