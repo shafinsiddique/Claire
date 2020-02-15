68890c36a00f085f8db292e8892cfcfbe9eda914
shafinsiddique <siddiqueshafin20@gmail.com>
2020-02-15 12:17:48 -0500

    more startercode for frontend.

--- frontend/view.py
+++ frontend/view.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
-@app.route('/',methods=['GET'])
-
+@app.route('/',methods=['GET','POST'])
 def home():
-    return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        pass # do something else.
+
 
