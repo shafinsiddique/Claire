ac4a35e1720871bd344d09eddd62ea115f27f04f
shafinsiddique <siddiqueshafin20@gmail.com>
2020-02-15 12:20:31 -0500

    added a paramter for the home page.

--- frontend/view.py
+++ frontend/view.py
@@ -3,8 +3,9 @@ from flask import Flask, render_template,request
 app = Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def home():
+    items = ["shafin","ripon"]
     if request.method == "GET":
-        return render_template("index.html")
+        return render_template("index.html", item=items)
     else:
         pass # do something else.
 
