62f3526fc42b97f877c1e651d04c97369cf6c3bc
Gogilan <gogilan8@gmail.com>
2020-02-15 20:02:25 -0500

    bare stuff.

--- frontend/view.py
+++ frontend/view.py
@@ -1,41 +1,39 @@
 from PIL import Image
 from flask import Flask, render_template, url_for, flash, redirect, request, app
 #from frontend import app, db, bcrypt
-from models import User, Post
+#from models import User, Post
 from datetime import datetime
 
-entries = [
-{'title': 'First Entry',
-'content': 'This is the content',
-'date': 'January 20, 2020'},
-{'title': 'Second Entry',
-'content': 'This is the content',
-'date': 'January 23, 2020'},
-]
 
-app = Flask(__main__)
+app = Flask(__name__)
 @app.route('/')
 
 @app.route("/home", methods = ['GET'])
-
 def home():
-    entries = entries
-    return render_template("entry.html", entries=entries)
 
-@app.route("/entry/new", methods = ['GET', 'POST'])
-def new_entry():
+    entries = [
+    {'title': 'First Entry',
+    'content': 'This is the content',
+    'date': 'January 20, 2020'},
+    {'title': 'Second Entry',
+    'content': 'This is the content',
+    'date': 'January 23, 2020'},
+    ]
+    return render_template("home.html", posts=entries)
+
+@app.route("/post/new", methods = ['GET', 'POST'])
+def new_post():
     # post=Post(title=form.title.data, content = form.content.data, date = datetime.now())
-
-    return render_template("entry.html")
+    return render_template("create_post.html")
 
 @app.route("/analytics")
-def entry():
+def analytics():
     return render_template("analytics.html")
 
-@app.route('/post/<post_id>')
-def post(post_id):
-    post = Post.query.get_or_404(post_id)
-    return render_template("post.html", title=post.title, post=post)
+ @app.route('/post/<post_id>')
+# def post(post_id):
+#     post = Post.query.get_or_404(post_id)
+#     return render_template("post.html", title=post.title, post=post)
 
 #routes,form for post
 
