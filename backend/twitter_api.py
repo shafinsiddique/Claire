2a1ac88267c8c5d35c3608dfb3a30e98ad96dd9c
riyasat <riyasattalukder@gmail.com>
2020-02-16 09:12:03 -0500

    Implmented the twitter api to get tweets

--- backend/twitter_api.py
+++ backend/twitter_api.py
@@ -1,15 +1,16 @@
 import tweepy
-from tweepy import Stream
-from tweepy import OAuthHandler
-from tweepy.streaming import StreamListener
 
-class twitter_streamer():
-
+def get_urls(keywords):
     auth = tweepy.OAuthHandler("tVbMbW31AeYh7cuUtk7IpvotF", "Ls7wvwDk2TNhzJ83C1B6an9dZiIinY8rHVBMJygeeZEJz3u3xV")
     auth.set_access_token("747290977216323584-pQ135YWmK4mHzRXmceXmK0RqeFfHchJ", "gLoVw2csmDWi7ndkGEE1DiwDH3dN1mQarxWxukUNdtSXg")
-    stream = Stream(auth,)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+    tweet_urls = []
+    for i in keywords:
+        tweets = tweepy.Cursor(api.search, q=i, lang="en").items(10)
+        for status in tweets:
+            for url in status.entities['urls']:
+                if 'twitter' in url['expanded_url']:
+                    tweet_urls.append(url['expanded_url'])
 
-    #def stream_tweets(self, filename, keywords):
-
-
+    return tweet_urls
 
